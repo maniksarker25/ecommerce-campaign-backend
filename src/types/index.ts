dc084c44884757d380ce73474a1b5c787c98ede9
manik <maniksarker652@gmail.com>
2026-06-27 10:51:43 +0600

    remove commetns adn work for stripe error handler

--- src/types/index.ts
+++ src/types/index.ts
@@ -5,8 +5,7 @@
 declare namespace Express {
   export interface Request {
     files?: {
-      product_image?: File[]; // Add more fields as needed, e.g. category_image, profile_image
-      // Add other fields you might expect to receive
+      product_image?: File[];
       profile_image?: File[];
       course_banner?: File[];
       banner?: File[];
